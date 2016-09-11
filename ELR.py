14708c7ca153925c2030cdfd6f085dba4c9a2f50
Chris Landa <stylesuxx@gmail.com>
2016-09-11 19:29:57 +0200

    Improved filtering of hits

--- ELR.py
+++ ELR.py
@@ -1,11 +1,39 @@
 from Source import Source
 from lxml import html
 import urllib
+import re
 
 
 class ELR(Source):
     baseUrl = 'http://e-liquid-recipes.com'
     searchUrl = '%s/%s' % (baseUrl, 'flavors/?%s')
+
+    def filterLinks(self, links, term):
+        # Filter away all hits where the first part of the term is only part of
+        # a word
+        terms = term.split(' ')
+        important = terms[0]
+        regex = r"" + re.escape(important) + "[ \n]"
+        links = filter(lambda link:
+                       re.search(regex, link['text'], re.IGNORECASE), links)
+
+        # If only one is left, we are done
+        if len(links) == 1:
+            return links[0]
+
+        # Filter away all hits that have more terms than the original term
+        linksSameLength = filter(lambda link:
+                                 len(link['text'].split(' ')) == len(terms),
+                                 links)
+
+        # Only apply the same length filter if somthing is left in the list
+        if linksSameLength:
+            links = linksSameLength
+
+        if links:
+            return links[0]
+
+        return None
 
     def getTopHit(self, term):
         params = urllib.urlencode({
@@ -17,12 +45,16 @@ class ELR(Source):
 
         f = urllib.urlopen(url)
         tree = html.fromstring(f.read())
-        text = tree.xpath('//table[contains(@class, "flavorlist")]'
-                          '/tbody/tr[1]/td[1]//a/text()')
-        link = tree.xpath('//table[contains(@class, "flavorlist")]'
-                          '/tbody/tr[1]/td[1]//a/@href')
 
-        if text and link:
-            return {'text': text[0], 'link': link[0]}
+        allHits = tree.xpath('//table[contains(@class, "flavorlist")]'
+                             '/tbody/tr/td[1]/span/a')
+        allLinks = map(lambda hit: {
+            'text': hit.text,
+            'link': hit.attrib['href']},
+            allHits)
+        link = self.filterLinks(allLinks, term)
+
+        if link:
+            return {'text': link['text'], 'link': link['link']}
 
         return None
