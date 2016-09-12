7e170dc9a174b43ed5bfba4d482c07d18de10f0a
Chris Landa <stylesuxx@gmail.com>
2016-09-13 00:12:45 +0200

    Moved username checks to bot. Added argparser for username and sub

--- Processor.py
+++ Processor.py
@@ -4,9 +4,8 @@ from ATF import ATF
 
 
 class Processor:
-    def __init__(self, pattern, username):
+    def __init__(self, pattern):
         self.pattern = pattern
-        self.username = username
         self.formatter = Markdown()
 
         self.elr = ELR()
@@ -31,25 +30,24 @@ class Processor:
                 print 'Processed submission: %s' % submission.id
 
     def comment(self, comment):
-        if comment.author.name != self.username:
-            matches = self.pattern.findall(comment.body)
-            if matches:
-                terms = self.buildSearchTerms(matches)
-                linksELR = map(lambda term: self.elr.getTopHit(term), terms)
-                linksATF = map(lambda term: self.atf.getTopHit(term), terms)
+        matches = self.pattern.findall(comment.body)
+        if matches:
+            terms = self.buildSearchTerms(matches)
+            linksELR = map(lambda term: self.elr.getTopHit(term), terms)
+            linksATF = map(lambda term: self.atf.getTopHit(term), terms)
 
-                links = {
-                    'ELR': map(lambda link:
-                               self.formatter.link(link), linksELR),
-                    'ATF': map(lambda link:
-                               self.formatter.link(link), linksATF)
-                }
+            links = {
+                'ELR': map(lambda link:
+                           self.formatter.link(link), linksELR),
+                'ATF': map(lambda link:
+                           self.formatter.link(link), linksATF)
+            }
 
-                if len(terms) > 0:
-                    reply = self.formatter.reply(terms, links)
-                    comment.reply(reply)
-                    # print reply
-                    print 'Processed comment: %s' % comment.id
+            if len(terms) > 0:
+                reply = self.formatter.reply(terms, links)
+                comment.reply(reply)
+                # print reply
+                print 'Processed comment: %s' % comment.id
 
     def buildSearchTerm(self, match):
         term = match.split('by')
--- bot.py
+++ bot.py
@@ -1,3 +1,4 @@
+import argparse
 import praw
 import time
 import re
@@ -5,20 +6,30 @@ import re
 from Processor import Processor
 
 userAgent = 'E-Juice Flavor Lookup 0.0.3 (by /u/stylesuxx)'
-username = 'flavor_bot'
-subreddit = 'test'
 sources = ['ELR', 'ATF']
 
 pattern = re.compile('\[\[([^\]]*)\]\]', re.MULTILINE)
 limitSubmissions = 25
 pauseSeconds = 60
 
+parser = argparse.ArgumentParser(description=userAgent)
+parser.add_argument('siteName', metavar='SITENAME',
+                    help='Site name to use from the config')
+parser.add_argument('-s', '--sub', dest='subreddit', metavar='SUB',
+                    default='test', help='Subreddit to monitor')
+
+args = parser.parse_args()
+siteName = args.siteName
+subreddit = args.subreddit
+
 processedSubmissions = []
 processedComments = []
-processor = Processor(pattern, username)
+processor = Processor(pattern)
 
-reddit = praw.Reddit(user_agent=userAgent, site_name=username)
+reddit = praw.Reddit(user_agent=userAgent, site_name=siteName)
 reddit.refresh_access_information()
+
+username = reddit.get_me()
 
 # Mark items and comments as processed, so we only care for new ones
 sub = reddit.get_subreddit(subreddit)
@@ -32,12 +43,14 @@ while True:
     try:
         sub = reddit.get_subreddit(subreddit)
         for submission in sub.get_new(limit=limitSubmissions):
-            if submission.id not in processedSubmissions:
+            if(submission.id not in processedSubmissions and
+               submission.author != username):
                 processor.submission(submission)
                 processedSubmissions.append(submission.id)
 
         for comment in sub.get_comments():
-            if comment.id not in processedComments:
+            if(comment.id not in processedComments and
+               comment.author != username):
                 processor.comment(comment)
                 processedComments.append(comment.id)
 
