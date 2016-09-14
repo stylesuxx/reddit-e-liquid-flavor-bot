a699f465395cffe0561c1c7b7e42f8db04580e73
Chris Landa <stylesuxx@gmail.com>
2016-09-15 01:02:58 +0200

    Renamed tests, improved sources tests

--- test/test_Sources.py
+++ test/test_Sources.py
@@ -1,5 +1,5 @@
 from nose.tools import assert_equal
-from flavor_bot.Source import ATF
+from flavor_bot.Source import ATF, ELR
 
 
 class TestSource:
@@ -27,16 +27,28 @@ class TestSource:
         assert_equal(hit['link'], ('https://alltheflavors.com/flavors/'
                                    'the-flavor-apprentice-strawberry-ripe'))
 
-    def test_atf_top_hit_with_prefied_vendor(self):
-        atf = ATF()
-
-        hit = atf.getTopHit('tpa Strawberry Ripe')
-        assert_equal(hit['text'], '(TPA) Strawberry (ripe)')
-        assert_equal(hit['link'], ('https://alltheflavors.com/flavors/'
-                                   'the-flavor-apprentice-strawberry-ripe'))
-
     def test_atf_top_hit_without_match(self):
         atf = ATF()
 
         hit = atf.getTopHit('Wiener Schnitzel')
+        assert_equal(hit, None)
+
+    def test_elr_top_hit_with_delimited_vendor(self):
+        elr = ELR()
+
+        hit = elr.getTopHit('Strawberry Ripe TPA')
+        assert_equal(hit['text'], 'Strawberry (Ripe) (TPA)')
+        assert_equal(hit['link'], 'http://e-liquid-recipes.com/flavor/5361')
+
+    def test_elr_top_hit_without_vendor(self):
+        elr = ELR()
+
+        hit = elr.getTopHit('Strawberry Ripe')
+        assert_equal(hit['text'], 'Strawberry ripe')
+        assert_equal(hit['link'], 'http://e-liquid-recipes.com/flavor/115914')
+
+    def test_elr_top_hit_without_match(self):
+        elr = ELR()
+
+        hit = elr.getTopHit('Wiener Schnitzel')
         assert_equal(hit, None)
